3fd42fe0710753b7bf78dfba07f3f37c1fad90ff
Jpiz <jpu0501@gmail.com>
2026-06-22 11:41:52 -0500

    jdhistorialmantenimientos

---
+++ CarWashWeb/jdHistorialMantenimiento.aspx.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Data
+Imports capaNegocio
+
+Partial Class jdHistorialMantenimiento
+    Inherits System.Web.UI.Page
+
+    Dim objCita As New clsCita()
+
+    Private Sub listar(ByVal dato As String)
+        Try
+            dgvMantenimientos.DataSource = objCita.listarHistorialCitasMantenimientoporDocumento(dato)
+            dgvMantenimientos.DataBind()
+        Catch ex As Exception
+            lblMensaje.CssClass = "mensaje-error"
+            lblMensaje.Text = "Error al listar mantenimientos: " & ex.Message
+        End Try
+    End Sub
+
+    Protected Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        listar(txtbuscador.Text.Trim())
+    End Sub
+End Class
