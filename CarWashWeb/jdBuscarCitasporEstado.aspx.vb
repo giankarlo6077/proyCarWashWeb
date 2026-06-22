da1c45a4988e28150dab75116642d73c717c5d1a
Jpiz <jpu0501@gmail.com>
2026-06-22 14:21:10 -0500

    jdBuscarCitasEstado

---
+++ CarWashWeb/jdBuscarCitasporEstado.aspx.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Data
+Imports capaNegocio
+
+Partial Class jdBuscarCitasporEstado
+    Inherits System.Web.UI.Page
+
+    Dim objCita As New clsCita()
+
+    Private Sub listar(ByVal dato As String)
+        Try
+            dgvGestion.DataSource = objCita.buscarporEstado(dato)
+            dgvGestion.DataBind()
+        Catch ex As Exception
+            lblMensaje.CssClass = "mensaje-error"
+            lblMensaje.Text = "Error al listar mantenimientos por estado: " & ex.Message
+        End Try
+    End Sub
+
+    Protected Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        listar(ddlSexo.SelectedItem.ToString())
+    End Sub
+End Class
